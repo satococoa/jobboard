3b446141aad349c34edf8543dde388e047368ee1
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-30 17:07:41 +0900

    implement groups (title only)

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,85 @@
+class GroupsController < ApplicationController
+  before_filter :require_login
+
+  # GET /groups
+  # GET /groups.json
+  def index
+    @groups = current_user.groups.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @groups }
+    end
+  end
+
+  # GET /groups/1
+  # GET /groups/1.json
+  def show
+    @group = current_user.groups.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @group }
+    end
+  end
+
+  # GET /groups/new
+  # GET /groups/new.json
+  def new
+    @group = current_user.groups.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @group }
+    end
+  end
+
+  # GET /groups/1/edit
+  def edit
+    @group = current_user.groups.find(params[:id])
+  end
+
+  # POST /groups
+  # POST /groups.json
+  def create
+    @group = current_user.groups.new(params[:group])
+
+    respond_to do |format|
+      if @group.save
+        format.html { redirect_to @group, notice: 'Group was successfully created.' }
+        format.json { render json: @group, status: :created, location: @group }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /groups/1
+  # PUT /groups/1.json
+  def update
+    @group = current_user.groups.find(params[:id])
+
+    respond_to do |format|
+      if @group.update_attributes(params[:group])
+        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /groups/1
+  # DELETE /groups/1.json
+  def destroy
+    @group = current_user.groups.find(params[:id])
+    @group.destroy
+
+    respond_to do |format|
+      format.html { redirect_to groups_url }
+      format.json { head :no_content }
+    end
+  end
+end
