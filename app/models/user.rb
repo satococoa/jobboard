9d39ad95c3f953af8968bfd82121f0c5c28db86b
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-30 16:35:20 +0900

    r g model group title user_id:integer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :icon_url, :name, :nickname, :provider, :uid
 
+  # relations
+  has_many :groups
+
   def self.find_or_create_by_auth_hash(auth)
     user = User.find_or_create_by_provider_and_uid(auth['provider'], auth['uid'])
     user.update_attributes(
