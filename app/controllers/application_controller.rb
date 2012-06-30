7bc8c7a474783938b97a603bd50a6f0756088b46
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-30 15:41:51 +0900

    implement create user/login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user, :logged_in?
+
+  private
+  def require_login
+    redirect_to '/auth/facebook' unless logged_in?
+  end
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if logged_in?
+  end
+  def logged_in?
+    session[:user_id].present?
+  end
 end
