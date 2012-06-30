480bc6c0daf55ba964423cf93e0b855619f42f61
Satoshi Ebisawa <e.satoshi@gmail.com>
2012-06-30 16:35:58 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120630072119) do
+ActiveRecord::Schema.define(:version => 20120630073515) do
+
+  create_table "groups", :force => true do |t|
+    t.string   "title"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "jobs", :force => true do |t|
     t.string   "title"
